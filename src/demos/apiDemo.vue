4307ab1c059f42e7c9ff44e59f7ac25201e2ca14
STAFF\zhangwei36 <1004695936@qq.com>
2017-07-31 18:02:36 +0800

    写了个api接口调用的demo

---
+++ src/demos/apiDemo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            APIDemo
+        </div>
+        <div>
+            {{fund}}
+        </div>
+        <div>
+            {{user}}
+        </div>
+    </div>
+</template>
+
+<script>
+//    导入api所在模块
+    import {FUND} from '../api/fund/fund';
+    import {USER} from '../api/user/user';
+    export default {
+        name: '',
+        components: {},
+        data () {
+            return {}
+        },
+        computed:{
+//            api模块方法执行之后返回的是一个在该模块下的api的集合
+            fund:function () {
+                return FUND().fundList
+            },
+            user:function () {
+                return USER()
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+
+</style>
