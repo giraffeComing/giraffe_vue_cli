b322d0ffdaf6e6413ad407de83cafc66eff422eb
STAFF\zhangwei36 <1004695936@qq.com>
2017-08-18 15:31:44 +0800

    模板字符串语法练习

--- src/views/exercise/exercise.vue
+++ src/views/exercise/exercise.vue
@@ -16,9 +16,39 @@
             VFooter,
             VContainer
         },
+        created(){
+            this.strTest01();
+            this.strTest02();
+            this.strTest03();
+            this.strTest04();
+            this.strTest05();
+        },
         data () {
             return {
-
+                name: 'paddy'
+            }
+        },
+        methods:{
+            strTest01:function () {
+                console.log(`string text line 1
+            string text line 2`);
+            },
+            strTest02:function () {
+                let name = "Bob", time = "today";
+                console.log(`Hello ${name}, how are you ${time}?`)
+            },
+            strTest03:function () {
+                console.log(`Hello ${this.name}, how are you?`)
+            },
+            fn:function () {
+                return "函数测试"
+            },
+            strTest04: function () {
+//                this.fn记得要加括号执行
+                console.log(`Hello ${this.fn()} fn`)
+            },
+            strTest05: function () {
+                console.log`测试`
             }
         }
     }
